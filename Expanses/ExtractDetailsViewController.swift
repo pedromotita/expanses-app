0db5ceaf31083c6e7abec71c8474aced70bc4acd
Pedro R. S. Mota <p.smota98@gmail.com>
2022-08-29 10:01:06 -0300

    Go to month detail on cell tap

---
+++ Expanses/ExtractDetailsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ExtractDetailsViewController.swift
+//  Expanses
+//
+//  Created by Pedro Mota on 29/08/22.
+//
+
+import UIKit
+
+class ExtractDetailsViewController: UIViewController {
+    
+    static let identifier = "ExtractDetailsViewController"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.largeTitleDisplayMode = .never
+    }
+}
