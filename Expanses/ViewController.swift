60039ee674002147321bd672ea56298a3b1ea141
Pedro R. S. Mota <p.smota98@gmail.com>
2022-09-16 12:30:45 -0300

    ViewCode setup

--- Expanses/ViewController.swift
+++ Expanses/ViewController.swift
@@ -8,36 +8,40 @@
 import UIKit
 
 class ExpanseListViewController: UIViewController {
-
-    @IBOutlet weak var tableView: UITableView!
-    
-    private var months: [String] = ["January", "February", "March"]
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        tableView.delegate = self
-        tableView.dataSource = self
-        
-        self.title = "Expanses"
+        view.backgroundColor = .systemRed
     }
+
+//    @IBOutlet weak var tableView: UITableView!
+//
+//    private var months: [String] = ["January", "February", "March"]
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        tableView.delegate = self
+//        tableView.dataSource = self
+//
+//        self.title = "Expanses"
+//    }
 }
 
-extension ExpanseListViewController: UITableViewDataSource, UITableViewDelegate {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.months.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let month = self.months[indexPath.row]
-        
-        if let cell = tableView.dequeueReusableCell(withIdentifier: ExpanseTableViewCell.identifier) as? ExpanseTableViewCell {
-            cell.configure(with: month)
-            return cell
-        }
-        return UITableViewCell()
-    }
-    
-}
-
+//extension ExpanseListViewController: UITableViewDataSource, UITableViewDelegate {
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return self.months.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let month = self.months[indexPath.row]
+//
+//        if let cell = tableView.dequeueReusableCell(withIdentifier: ExpanseTableViewCell.identifier) as? ExpanseTableViewCell {
+//            cell.configure(with: month)
+//            return cell
+//        }
+//        return UITableViewCell()
+//    }
+//
+//}
+//
